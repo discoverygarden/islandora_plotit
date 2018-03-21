4150f9d52fe0e892721bf9fb1bb6c9f757780fea
Nia Kathoni <nikathone@gmail.com>
2018-03-21 16:58:46 +0000

    added dtoc form for customization.

---
+++ js/voyant_dtoc.js
@@ -0,0 +1,19 @@
+(function ($) {
+  Drupal.behaviors.ViyantDtocFormSingleSubmit = {
+    attach: function attach() {
+      function onFormSubmit(e) {
+        var $form = $(e.currentTarget);
+        var formValues = $form.serialize();
+        var previousValues = $form.attr('data-drupal-form-submit-last');
+        if (previousValues === formValues) {
+          e.preventDefault();
+        }
+        else {
+          $form.attr('data-drupal-form-submit-last', formValues);
+        }
+      }
+
+      $('body').once('form-single-submit').on('submit.singleSubmit', 'form:not([method~="GET"])', onFormSubmit);
+    }
+  };
+})(jQuery);
